6b836144956be6190571587a7c88605b0ef5c371
ridhhere <ridhhere@gmail.com>
2023-03-24 17:33:14 -0400

    Renaming the Project

--- src/main/java/com/ridh/OnlineShoppingManagement.java
+++ src/main/java/com/ridh/OnlineShoppingManagement.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductManagementApplication {
+public class OnlineShoppingManagement {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductManagementApplication.class, args);
+		SpringApplication.run(OnlineShoppingManagement.class, args);
 	}
 
 }
--- src/test/java/com/ridh/OnlineShoppingManagementTests.java
+++ src/test/java/com/ridh/OnlineShoppingManagementTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductManagementApplicationTests {
+class OnlineShoppingManagementTests {
 
 	@Test
 	void contextLoads() {
